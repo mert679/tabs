dd3fcb7e4086e2a716f5f43b14c7358894993d3c
mert679 <64027475+mert679@users.noreply.github.com>
2021-04-12 22:31:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const tabs=document.querySelectorAll("[data-tab-target]");//home,pricing,about seçtim
+const tabs=document.querySelectorAll(".tabs");//home,pricing,about seçtim
 const tabContents=document.querySelectorAll("[data-tab-content]");//contentleri seçtim
 // bunun ile loop yaptım
 tabs.forEach(tab=>{
   tab.addEventListener("click",()=>{
-    const target=document.querySelector(tab.dataset.tabTarget);
+    const target=document.querySelector(tab.dataset.tabTarget);// bunun sayesinde seçtiğim tag specificleşti
     tabContents.forEach(tabContent=>{
       tabContent.classList.remove("active")
     })
     tabs.forEach(tab=>{
       tab.classList.remove("active")
-    })
+    });
     tab.classList.add("active")
     target.classList.add("active")
     
